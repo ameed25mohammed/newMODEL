8a7fa26dddb599be020a68b85c8e585c2fadc431
ameed25mohammed <134552306+ameed25mohammed@users.noreply.github.com>
2025-07-25 05:11:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import numpy as np
 import joblib
 import os
@@ -10,6 +11,7 @@ except:
     model = None
 
 app = Flask(__name__)
+CORS(app)  # إضافة CORS
 
 @app.route('/', methods=['GET'])
 def home():
